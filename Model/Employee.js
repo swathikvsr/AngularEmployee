04f199113bb1911895aec1bf239d8cf416a26637
swathikvsr <swathi.kudumula@gmail.com>
2017-05-14 19:39:21 +1200

    remove js files

--- Model/Employee.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Employee = (function () {
-    function Employee() {
-    }
-    return Employee;
-}());
-exports.Employee = Employee;
-//# sourceMappingURL=employee.js.map
