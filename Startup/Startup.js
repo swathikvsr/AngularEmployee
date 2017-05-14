f67b2cdb437e149a4a2d8cf82b1a8947ec4712ad
swathikvsr <swathi.kudumula@gmail.com>
2017-05-14 19:41:57 +1200

    remove all js file

--- Startup/Startup.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
-var MainModule_1 = require("../Modules/MainModule");
-var core_1 = require("@angular/core");
-var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-core_1.enableProdMode();
-platform.bootstrapModule(MainModule_1.MainModule);
-//# sourceMappingURL=Startup.js.map
